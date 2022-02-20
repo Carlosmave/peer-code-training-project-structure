29e3020b353845fd068d570dbe3f58c22a470c17
Carlos Martinez <carlosm28@outlook.com>
2022-02-20 10:31:50 -0500

    Google Test 2

--- libraries/google/google.py
+++ libraries/google/google.py
@@ -1,5 +1,5 @@
 from libraries.common import act_on_element, capture_page_screenshot, log_message
-from config import OUTPUT_FOLDER
+from config import OUTPUT_FOLDER, tabs_dict
 import random
 import time
 
@@ -15,8 +15,18 @@ class Google():
         Access Google from the browser.
         """
         self.browser.go_to(self.google_url)
+        try:
+            frame = act_on_element('//*[@id="cnsw"]/iframe', "find_element")
+            self.browser.switch_to.frame(frame)
+            act_on_element('//*[@id="introAgreeButton"]', "click_element")
+        except Exception as e:
+            print(e)
+            pass
+        self.browser.switch_window(locator = self.browser.get_window_handles()[tabs_dict["Google"]])
 
     def search_movie(self):
+        log_message("Start - Search Movie")
         self.browser.input_text_when_element_is_visible('//input[@title="Search"]', "Spider-Man")
         act_on_element('//div[@class="CqAVzb lJ9FBc"]//input[@value="Google Search"]', "click_element")
         time.sleep(5)
+        log_message("End - Search Movie")
